c28f37c1a6fe433bf9c5bbf1c61556e3ee7f1b70
2jh1230 <2jh1230@naver.com>
2025-05-20 13:55:12 +0900

    .

--- Memordo_Extension-main/background/background.js
+++ Memordo_Extension-main/background/background.js
@@ -22,9 +22,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
     chrome.storage.local.get(['visitedUrls'], (localData) => {
       const visited = localData.visitedUrls || [];
-
+      // const normalizedNewUrl = normalizeUrl(tab.url);
       // URL 중복 확인 (기존과 동일)
-      const alreadyVisited = visited.some(entry => entry.url === tab.url);
+      const alreadyVisited = visited.some(entry => normalizeUrl(entry.url) === normalizedNewUrl);
       if (!alreadyVisited) {
         // title 추가
         visited.push({
@@ -42,6 +42,17 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   });
 });
 
+//URL 정규화 함수
+// function normalizeUrl(url) {
+//   try {
+//     const parsed = new URL(url);
+//     const ignoredParams = ['page', 'timestamp'];
+//     ignoredParams.forEach(param => parsed.searchParams.delete(param));
+//     return parsed.origin + parsed.pathname;
+//   } catch (e) {
+//     return url;
+//   }
+// }
 
 // 인증 토큰 받기
 function getAuthToken(callback) {
